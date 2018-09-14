d318dadeaa99fd0a8c524d159bffce0c168f1194
d3rbastl3r <igor.martens.83@gmail.com>
2018-09-14 21:31:14 +0200

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+ * A minimal example how to push data (parallel) to 8bit shift register with ATTiny13A
+ *
+ * @author Igor Martens
+ * @since 14.09.2018
+ */
+
+#define F_CPU 960000
+
+#include <avr/io.h>
+void setup() {
+}
+
+int main(void) {
+    setup();
+
+    while(1) {
+    }
+
+    return 0;
+}
